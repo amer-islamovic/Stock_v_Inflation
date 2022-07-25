a45ca9385821e27d5788d28990fb3d603de64f30
Amer Islamovic <aislamovic01@gmail.com>
2022-07-25 00:57:05 -0400

    Added overall value based on  investment

--- StockCalc.py
+++ StockCalc.py
@@ -21,7 +21,7 @@ for folder, subfolders, files in os.walk('Stock_Growth/csv_files/'):
             path = os.path.join(folder, file) 
             os.remove(path)
 
-header = ['sort', 'timestamp', 'open', 'high', 'low' , 'close' , 'adjusted close' , 'volume' , 'dividend amount' , 'ticker']
+header = ['timestamp', 'open', 'high', 'low' , 'close' , 'adjusted close' , 'volume' , 'dividend amount' , 'ticker' , 'current value']
 
 with open('Stock_Growth/csv_files/temp.csv', 'w', encoding='UTF8') as f:
     writer1 = csv.writer(f)
@@ -58,36 +58,66 @@ for SYMB in SYMBs:
     # Pull csv and save as "temp_stk"
     stk = pd.read_csv("https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=" + SYMB +"&apikey=" + alpha_key + "&datatype=csv")  
     stk.head()
-    stk.to_csv('Stock_Growth/csv_files/temp_stk.csv')
+    stk.to_csv('Stock_Growth/csv_files/temp_stk.csv', index=False)
 
     # Add column with stocks' ticker
     ticker = SYMB
     add_column_in_csv('Stock_Growth/csv_files/temp_stk.csv', 'Stock_Growth/csv_files/temp_stk1.csv', lambda row, line_num: row.append(ticker))
+    
+    # Sort by date
+    csvData = pd.read_csv("Stock_Growth/csv_files/temp_stk1.csv")
+    csvData['timestamp'] = csvData['timestamp'].astype('datetime64[ns]')
+    csvData.sort_values(by='timestamp', inplace=True)
+
+    csvData.to_csv("Stock_Growth/csv_files/temp_stk2.csv", index=False)
+
+    # Calculate Divident Growth
+    base_value = 1
+    c = []
+    df1 = pd.read_csv('Stock_Growth/csv_files/temp_stk2.csv')
+    for i in range(len(df1['dividend amount'])):
+        base_value = base_value / df1['close'][i] * df1['dividend amount'][i]  + base_value
+        c.append(base_value)
+    df1['current value'] = c
+    df1.to_csv('Stock_Growth/csv_files/temp_stk3.csv', index=False)
 
     # Append each individual stock pull to master "temp.csv" file
-    csv_list = ['Stock_Growth/csv_files/temp_stk1.csv']
+    csv_list = ['Stock_Growth/csv_files/temp_stk3.csv']
    
     for csv_file in csv_list:
         df = pd.read_csv(csv_file)
         df.to_csv('Stock_Growth/csv_files/temp.csv', mode ='a', header=False, index=False)
 
-# Sort data by ticker, date(descending)
-# csvData = pd.read_csv("Stock_Growth/csv_files/temp.csv")
-# csvData.sort_values(["ticker", "sort"], 
-#                     axis=0,
-#                     ascending=[True, False], 
-#                     inplace=True)
-# csvData.to_csv("Stock_Growth/csv_files/temp1.csv")
+#Delete unnecessary columns
+# with open("Stock_Growth/csv_files/temp.csv", "r") as source:
+#     reader = csv.reader(source)
+      
+#     with open("Stock_Growth/csv_files/temp2.csv", "w") as result:
+#         writer = csv.writer(result)
+#         for r in reader:
+#             writer.writerow((r[3], r[7], r[10], r[11], r[12]))
 
-# Sort by date WIP
-csvData = pd.read_csv("Stock_Growth/csv_files/temp.csv")
-# csvData['timestamp'] = pd.to_datetime(csvData.date, infer_datetime_format = True)
-# csvData.sort_values(["ticker"],by = 'timestamp', ascending = True, inplace = True)
-csvData = sorted(csvData, key = lambda row: dt.datetime.strptime(row[1], "%Y-%m-%d"))
-csvData.to_csv("Stock_Growth/csv_files/temp1.csv")
+#Delete blank rows
+df = pd.read_csv('Stock_Growth/csv_files/temp.csv')
+df.dropna(axis=0, how='all',inplace=True)
+df.to_csv('Stock_Growth/csv_files/temp1.csv', index=False)
+
+data = pd.read_csv('Stock_Growth/csv_files/temp1.csv')
+df.head(2)
+df=df.drop(['open', 'high', 'low' , 'adjusted close' , 'volume'],axis=1)
+
+df.to_csv('Stock_Growth/csv_files/temp2.csv', index=False)
+
 
 # Inflation USD - Pull and save inflation data for US as "temp_inf.csv"
 infl = pd.read_csv("https://www.alphavantage.co/query?function=INFLATION&apikey=" + alpha_key + "&datatype=csv")   
 infl.head()
 
 infl.to_csv('Stock_Growth/csv_files/temp_inf.csv')
+
+# Delete rows
+data1 = pd.read_csv('Stock_Growth/csv_files/temp_inf.csv')
+df = pd.DataFrame(data1)
+df=df.drop(df.index[25:65], inplace=True)
+
+df.to_csv('Stock_Growth/csv_files/temp_inf1.csv', index=False)
